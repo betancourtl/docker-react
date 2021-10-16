11e7fb82dada0a961d1deb48151d993c855e94b8
Luis Betancourt <webdeveloperpr@gmail.com>
2021-10-16 11:40:25 -0400

    update scripts and create travis file

--- script.sh
+++ script.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
 
-docker build \
-  -t betancourtl/node-app:latest \
-  -f Dockerfile.dev .
+function build-app() {
+  docker build \
+    -t betancourtluis/docker-react:latest \
+    -f Dockerfile.dev .
+}
 
-docker run \
-  -it \
-  --rm \
-  -p 3000:3000 \
-  -v /app/node_modules \
-  -v $(pwd):/app \
-  betancourtl/node-app:latest
+function run-app() {
+  docker run \
+    -it \
+    --rm \
+    -p 3000:3000 \
+    -v /app/node_modules \
+    -v $(pwd):/app \
+    betancourtluis/docker-react:latest
+}
+
+function test-app() {
+  docker run \
+    --rm \
+    -p 3000:3000 \
+    -v /app/node_modules \
+    -v $(pwd):/app \
+    -e CI=true \
+    betancourtluis/docker-react:latest \
+    npm run test -- --coverage
+}
 
 function build-nginx() {
   docker build \
-    -t betancourtl/nginx:latest \
+    -t betancourtluis/nginx:latest \
     -f Dockerfile .
 }
 
@@ -23,5 +38,5 @@ function nginx() {
     -it \
     --rm \
     -p 8080:80 \
-    betancourtl/nginx:latest
+    betancourtluis/nginx:latest
 }
