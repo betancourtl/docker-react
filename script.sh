78caf98cc367e3b1a82b6c0cc422b62679f579a5
Luis Betancourt <webdeveloperpr@gmail.com>
2021-10-16 17:53:57 -0400

    Update docker-compose.yml file with correct ports

--- script.sh
+++ script.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
 
-function build-app() {
+# DEV
+function build-dev-app() {
   docker build \
-    -t betancourtluis/docker-react:latest \
+    -t betancourtluis/docker-react-dev:latest \
     -f Dockerfile.dev .
 }
 
-function run-app() {
+function run-dev-app() {
   docker run \
     -it \
     --rm \
     -p 3000:3000 \
     -v /app/node_modules \
     -v $(pwd):/app \
-    betancourtluis/docker-react:latest
+    betancourtluis/docker-react-dev:latest
+}
+
+# PROD
+function build-prod-app() {
+  docker build \
+    -t betancourtluis/docker-react-prod:latest \
+    -f Dockerfile .
+}
+
+function run-prod-app() {
+  docker run \
+    -it \
+    --rm \
+    -p 80:80 \
+    -v /app/node_modules \
+    -v $(pwd):/app \
+    betancourtluis/docker-react-prod:latest
 }
 
 function test-app() {
